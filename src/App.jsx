ac535e28696d0b64b03bca9139e767fc1393bf44
Stella Hangi <stellahjames200@gmail.com>
2025-04-16 16:30:06 +0300

    my initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"; // Import Link for routing
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faChurch, faStaffSnake } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faChildren, faChurch, faGamepad, faNotesMedical, faPeopleRoof, faPersonDigging, faShieldHeart, faStaffSnake, faWarehouse } from '@fortawesome/free-solid-svg-icons'
 import morning from "./assets/mainlogo.png";
 import health from "./assets/health1.jpg";
+import health1 from "./assets/healthcare.png"
+import health2 from "./assets/health2.jpeg"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
+
+
 
 
 function App() {
+
   const [navOpen, setNavOpen] = useState(false); // State for mobile menu
 
   return (
@@ -46,7 +56,7 @@ function App() {
       href="#services"
       className="bg-green-800 text-lime-50 px-6 py-3 rounded-lg hover:bg-green-500 transition duration-300 inline-block"
     >
-      Sign in
+      Donate Now
     </a>
               </li>
             </ul>
@@ -75,16 +85,17 @@ function App() {
       href="#services"
       className="bg-green-800 text-lime-50 px-6 py-3 rounded-lg hover:bg-green-500 transition duration-300 inline-block"
     >
-      Discover Our Services
+      See Our Projects
     </a>
   </div>
 </section>
 
 
       {/* About Section */}
-      <section id="about" className="py-16 bg-white px-4">
+      <section id="about" className="py-16 bg-white px-16">
+      <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         <div className="max-w-4xl mx-auto text-center">
-          <h3 className="text-3xl font-semibold mb-6">About Us</h3>
+          <h3 className="text-2xl font-bold mb-6">About Us</h3>
           <p className="text-lg text-gray-700 leading-relaxed text-justify">
           Afya Ustawi Foundation was established in 2025, this is a non-governmental organization that empowers the community in gaining more access toward matters conserning their health and social welfare,
            The organization  operates in Dodoma and Dar es salaam, Afya Ustawi 
@@ -95,45 +106,170 @@ function App() {
           three enemies as one can be a causative and consequence of the other and the circle goes on. 
            </p>
         </div>
+        <img src={health1} alt="" />
+        </div>
         <div className="mb-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 px-5 py-5 space-y-2 ">
             <div className="bg-white shadow-lg rounded-lg p-5 items-center">
-              <h4 className="text-xl font-semibold text-center mb-4 text-green-800">OUR MISSION</h4>
+              <h4 className="text-xl font-semibold text-center mb-4 text-green-950">Our Mission</h4>
               <p className="text-gray-700 text-center">empowering people to gain more control over their health” 
                 we serve to empower communities with resources 
                 and skills that enable them to live heathier life styles through sustainable and community 
                 driven initiatives, bridging the gap between ignorance, diseases, and poverty.</p>
             </div>
             <div className="bg-white shadow-lg rounded-lg p-6">
-              <h4 className="text-xl font-semibold mb-4 text-center text-green-800">OUR VISION</h4>
+              <h4 className="text-xl font-semibold mb-4 text-center text-green-950">Our Vision</h4>
               <p className="text-gray-700 text-center">being the leading force in creating a future where people are empowered to live healthier, 
                 more fulfilling lives.</p>
             </div>
             <div className="bg-white shadow-lg rounded-lg p-6">
-              <h4 className="text-xl font-semibold mb-4 text-center text-green-800">OUR VALUES</h4>
+              <h4 className="text-xl font-semibold mb-4 text-center text-green-950">Our Values</h4>
               <p className="text-gray-700 text-center">.Quality of care, financial responsibility, Empathy</p>
               <span><p className="text-gray-700 text-center">.learning, Intergrity, inclusion, Resilience, Health sustainability.</p></span>
             </div>
             </div>
+
+            {/* our history */}
+            <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 pt-6 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <img src={health2} alt="" />
+        <div className="max-w-4xl mx-auto text-center">
+          <h3 className="text-2xl font-bold mb-6">Our History</h3>
+          <p className="text-lg text-gray-700 leading-relaxed text-justify">
+          
+Magreth is a founder and Managing Director of Afya Ustawi Foundation. 
+By addressing the unique challenges faced by households in Dodoma and Dar es salaam Tanzania many of whom
+live below the poverty line and limited access to medical services, missing opportunity to fulfilling lives.
+She begun her mission to  seeking to create opportunities for healthier, more informed, and resilient
+communities.
+
+          </p>
+          <p className="text-lg text-gray-700 leading-relaxed text-justify">
+          So she developed the passion of being an ambassador for healthier communities for people living with disabilities and most vulnerable communities, taking
+cognizant of the suffering of households in Dodoma and Dar es salaam Tanzania, who live below the poverty
+line; yet, compounded by numerous needs to reach full potential and live fulfilling lives.
+ 
+           </p>
+        </div>
+       
+        </div>
+            
+           
       </section>
 
       {/* Services Section */}
       <section id="services" className="bg-gray-50 py-16 px-4">
         <div className="max-w-6xl mx-auto text-center">
-          <h3 className="text-3xl font-semibold mb-6">Our Services</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div className="bg-white shadow-lg rounded-lg p-6">
-              <h4 className="text-xl font-semibold mb-4">Sunday Worship</h4>
-              <p className="text-gray-700">Join us every Sunday for an uplifting time of worship and teaching.</p>
-            </div>
-            <div className="bg-white shadow-lg rounded-lg p-6">
-              <h4 className="text-xl font-semibold mb-4">Prayer Meetings</h4>
-              <p className="text-gray-700">We gather for prayer every Wednesday evening to support each other.</p>
-            </div>
-            <div className="bg-white shadow-lg rounded-lg p-6">
-              <h4 className="text-xl font-semibold mb-4">Youth Fellowship</h4>
-              <p className="text-gray-700">A special gathering for the youth to grow in faith and community.</p>
-            </div>
-          </div>
+          <h3 className="text-2xl font-bold mb-6">Our Projects</h3>
+          <Swiper
+  modules={[Autoplay]}
+  autoplay={{ delay: 2000, disableOnInteraction: false }}
+  loop={true}
+  spaceBetween={20}
+  breakpoints={{
+    0: { slidesPerView: 1 },
+    640: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  }}
+>
+          <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faChildren} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">Rafiki Wa Kweli</h4>
+        <p className="text-gray-700">Rafiki wa kweli donate to support CBR networks.</p>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  {/* Slide 2 */}
+  <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faPersonDigging} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">Kazi Ni Afya</h4>
+        <p className="text-gray-700">work integration to mentor health empowerment and health education.</p>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  {/* Slide 3 */}
+  <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faChildren} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">Ndugu Tusaidiane</h4>
+        <p className="text-gray-700">Empowering local community to a local enterprise that will support most vulnerable groups in the community,
+          a road to a self-sustainable community.</p>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faShieldHeart} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">Pendezesha</h4>
+        <p className="text-gray-700">Healthy environment support human health and reduce risk of diseases.</p>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faGamepad} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">Michezo Advocacy</h4>
+        <p className="text-gray-700">Advocating for health issue vie sports and games.</p>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faPeopleRoof} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">University Youth Club Mentorships</h4>
+        <p className="text-gray-700">Mentoring young professions.</p>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faWarehouse} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">Capacity Building</h4>
+        <p className="text-gray-700">For health care workers on public health .</p>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <div className="bg-white shadow-lg rounded-lg p-5 px-10">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-20 h-20 flex justify-center items-center bg-green-50 shadow-xl rounded-full mb-3">
+          <FontAwesomeIcon icon={faNotesMedical} className="text-3xl text-green-800 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold mb-3 text-green-950">Healthy Education</h4>
+        <p className="text-gray-700">Educating communities for healthier tomorrows.</p>
+      </div>
+    </div>
+  </SwiperSlide>
+</Swiper>
         </div>
       </section>
 
@@ -153,7 +289,7 @@ function App() {
       {/* Footer */}
       <div className="bg-gray-100">
       <footer className="bg-green-700 text-white py-6 text-center">
-        <p>&copy; 2024 Church Name. All Rights Reserved.</p>
+        <p>&copy; 2025 Afya Ustawi Foundation. All Rights Reserved.</p>
       </footer>
       </div>
     </div>
